a08115db172446f95629926c46bf95d8047e3223
GabsLeo <78516413+Gabs-Leo@users.noreply.github.com>
2022-06-05 20:54:30 -0300

    :sparkles: change URL

--- src/services/URLS.ts
+++ src/services/URLS.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8080"
+export const BASE_URL = "https://gabs-portfolio.herokuapp.com"
 export const GET_IMAGE_URL = (page:string) => `${BASE_URL}/api/v1/${page}/images`;
