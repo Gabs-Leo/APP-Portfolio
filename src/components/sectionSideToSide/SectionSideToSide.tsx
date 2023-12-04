57cb241a2dab0b7f21f5ca6fd9d4e7c798ad44f0
gabriel-scaled <gabrieldemoraistaveira@gmail.com>
2023-12-04 13:01:28 -0300

    changed bars for list

--- src/components/sectionSideToSide/SectionSideToSide.tsx
+++ src/components/sectionSideToSide/SectionSideToSide.tsx
@@ -17,14 +17,21 @@ export const SectionSideToSide = () => {
                 <div className="text">
                     <h3 data-aos="fade-left" data-aos-duration="1600">Oi, eu sou o <strong>Gabs!</strong></h3>
                     <p data-aos="fade-left" data-aos-duration="2000">Meu nome é Gabriel, tenho {(2003 - new Date().getFullYear())*-1} anos, moro em Franca-Sp (Brasil). Atualmente estou cursando Análise e Desenvolvimento de Sistemas (Fatec Franca-SP) e fazendo o curso online de desenvolvimento de Games da Danki Code.</p>
-                    <div data-aos="fade-left" data-aos-duration="2400">
-                        <ProgressBar title="Spring Boot" percentage={90}/>
-                        <ProgressBar title="GitHub" percentage={75}/>
-                        <ProgressBar title="React Js" percentage={70}/>
-                        <ProgressBar title="Bootstrap" percentage={70}/>
-                    </div>
+                    <ul data-aos="fade-left" data-aos-duration="2400">
+                        <li>Google Cloud e AWS</li>
+                        <li>Docker, Kubernetes e Helm</li>
+                        <li>CI/CD (Github Actions, Gitlab-CI, AWS Codepipeline)</li>
+                        <li>Terraform e Cloudformation</li>
+                        <li>Nginx</li>
+                        <li>Linux e Bash Script</li>
+                        <li>JavaScript, TypeScript, Python, Java</li>
+                        <li>Rest APIs com Spring Boot e NodeJs</li>
+                        <li>MySQL, PostgreSQL e SQL Server</li>
+                        <li>ReactJs com Bootstrap</li>
+                    </ul>
                     <br />
                     <div data-aos="fade-left" data-aos-duration="2400">
+                        <br/>
                         <MyButton text="Contato" link="#contact"/>
                     </div>
                 </div>
@@ -33,3 +40,13 @@ export const SectionSideToSide = () => {
     </section>
     )
 }
+
+/*
+<div data-aos="fade-left" data-aos-duration="2400">
+    <ProgressBar title="Java com Spring Boot" percentage={90}/>
+    <ProgressBar title="Spring Boot" percentage={90}/>
+    <ProgressBar title="GitHub" percentage={75}/>
+    <ProgressBar title="React Js" percentage={70}/>
+    <ProgressBar title="Bootstrap" percentage={70}/>
+</div>
+*/
